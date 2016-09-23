484e32cb234ba303c391c23c9dd73d0c0c2f1cbe
LaurentFer <laurent.ferrer@me.com>
2016-09-23 08:09:12 +0200

    Refactoring

--- src/model/Start.java
+++ src/model/Start.java
@@ -1,11 +1,20 @@
 package model;
 
+import java.sql.SQLException;
+
 public class Start {
 
-	public static void main(String[] args){
+	public static void main(String[] args) throws SQLException{
+		
+		//Création de l'instance unique de la connection (singleton)
 		MaConnection myCon 	= MaConnection.getMaConnection();
-		MaConnection myCon2 = MaConnection.getMaConnection();
-		System.out.println(myCon+"/"+myCon2);
+		System.out.println("Adresse de l'objet : "+ myCon);
+		System.out.println("Connexion inactive :"+ myCon.isNotConnexionActive());
+
+		//Fermeture de l'instance unique de la connection
+		myCon.FermerConnexion();
+		System.out.println("Adresse de l'objet : "+ myCon);
+		System.out.println("Connexion inactive :"+ myCon.isNotConnexionActive());
 	}
 	
 }
